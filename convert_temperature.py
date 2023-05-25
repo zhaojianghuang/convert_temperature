a17a885fbb13cc3dee71d5c31d51441ed8d11eb5
zhaojianghuang <zhaojiang.huang95@gmail.com>
2023-05-25 14:22:58 +0200

    docstring for source code documentation

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,3 +1,13 @@
 def fahrenheit_to_celsius(temperature_fahrenheit):
+	"""
+	Converts temperature in fahrenheit to celsius
+
+	Parameters:
+	temperature_fahrenheit
+	
+	Returns:
+	temperature_celsius
+
+	"""
 	temp_celsius = (temperature_fahrenheit - 32) * (5.0/9.0)
 	return temp_celsius
